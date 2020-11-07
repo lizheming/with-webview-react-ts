b8cf8b5dc36db351392f5e7d62de6ae752d86af5
lizheming <i@imnerd.org>
2020-11-07 14:42:09 +0800

    chore: add typescript

---
+++ template/src/my-command.ts
@@ -0,0 +1,35 @@
+/// <reference types="sketch-app-types" />
+
+import BrowserWindow, { BrowserWindowOptions } from 'sketch-module-web-view';
+import UI from 'sketch/ui';
+
+const webviewIdentifier = 'template.webview';
+
+export default function () {
+  const options:BrowserWindowOptions = {
+    identifier: webviewIdentifier,
+    width: 240,
+    height: 180,
+    show: false
+  }
+
+  const browserWindow = new BrowserWindow(options)
+
+  // only show the window when the page has loaded to avoid a white flash
+  browserWindow.once('ready-to-show', _ => browserWindow.show());
+
+  const webContents = browserWindow.webContents;
+
+  // print a message when the page loads
+  webContents.on('did-finish-load', _ => UI.message('UI loaded!'));
+
+  // add a handler for a call from web content's javascript
+  webContents.on('nativeLog', s => {
+    UI.message(s);
+    webContents
+      .executeJavaScript(`setRandomNumber(${Math.random()})`)
+      .catch(console.error);
+  });
+
+  browserWindow.loadURL(require('../resources/webview.html'))
+}
--- template/webpack.skpm.config.js
+++ template/webpack.skpm.config.js
@@ -2,31 +2,40 @@ module.exports = function (config, entry) {
   config.node = entry.isPluginCommand ? false : {
     setImmediate: false
   };
-  config.module.rules.push({
-    test: /\.(html)$/,
-    use: [{
-        loader: "@skpm/extract-loader",
-      },
-      {
-        loader: "html-loader",
-        options: {
-          attrs: [
-            'img:src',
-            'link:href'
-          ],
-          interpolate: true,
+  
+  config.resolve.extensions = config.resolve.extensions.concat(['.ts', '.tsx']);
+  config.module.rules = config.module.rules.concat([
+    {
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader'
+    },
+    {
+      test: /\.(html)$/,
+      use: [{
+          loader: "@skpm/extract-loader",
         },
-      },
-    ]
-  })
-  config.module.rules.push({
-    test: /\.(css)$/,
-    use: [{
-        loader: "@skpm/extract-loader",
-      },
-      {
-        loader: "css-loader",
-      },
-    ]
-  })
+        {
+          loader: "html-loader",
+          options: {
+            attrs: [
+              'img:src',
+              'link:href'
+            ],
+            interpolate: true,
+          },
+        },
+      ]
+    },
+    {
+      test: /\.(css)$/,
+      use: [{
+          loader: "@skpm/extract-loader",
+        },
+        {
+          loader: "css-loader",
+        },
+      ]
+    }
+  ]);
 }
